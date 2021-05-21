ed2bc6674504077a60e30559d1d0e6915daa48ff
fernie-cpu <yoloisjustanick@gmail.com>
2021-05-21 19:01:20 -0300

    Director detail page controller

--- controllers/director_controller.js
+++ controllers/director_controller.js
@@ -17,8 +17,34 @@ exports.director_list = (req, res, next) => {
     });
 };
 
-exports.director_detail = (req, res) => {
-  res.send('director detail page: ' + req.params.id);
+exports.director_detail = (req, res, next) => {
+  async.parallel(
+    {
+      director: (callback) => {
+        Director.findById(req.params.id).exec(callback);
+      },
+      director_movies: (callback) => {
+        Movie.find({ director: req.params.id }, 'title synopsis').exec(
+          callback
+        );
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      if (results.director == null) {
+        let err = new Error('Director not found');
+        err.status = 404;
+        return next(err);
+      }
+      res.render('director_detail', {
+        title: `${results.director.name} - Movie Collection`,
+        director: results.director,
+        director_movies: results.director_movies,
+      });
+    }
+  );
 };
 
 exports.director_create_get = (req, res) => {
