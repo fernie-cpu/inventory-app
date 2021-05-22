0d6e3d3b3e27eeb8753cc2781534c1603f4159c2
fernie-cpu <yoloisjustanick@gmail.com>
2021-05-21 23:01:30 -0300

    form to create director

--- controllers/director_controller.js
+++ controllers/director_controller.js
@@ -1,5 +1,6 @@
 const Director = require('../models/director');
 const Movie = require('../models/movie');
+const { body, validationResult } = require('express-validator');
 
 var async = require('async');
 
@@ -47,13 +48,76 @@ exports.director_detail = (req, res, next) => {
   );
 };
 
-exports.director_create_get = (req, res) => {
-  res.send('create director get');
+exports.director_create_get = (req, res, next) => {
+  res.render('director_form', { title: 'Create Director' });
 };
 
-exports.director_create_post = (req, res) => {
-  res.send('create director post');
-};
+exports.director_create_post = [
+  body('first_name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('First name must be specified.')
+    .isAlphanumeric()
+    .withMessage('First name has non-alphanumeric characters.'),
+  body('family_name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('First name must be specified.')
+    .isAlphanumeric()
+    .withMessage('First name has non-alphanumeric characters.'),
+  body('date_of_birth', 'Invalid date of birth')
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  body('date_of_death', 'Invalid date of birth')
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  body('place_of_birth')
+    .isLength({ min: 1 })
+    .escape()
+    .isAlphanumeric()
+    .withMessage('Place of birth has non-alphanumeric characters'),
+  body('place_of_death')
+    .escape()
+    .isAlphanumeric()
+    .withMessage('Place of birth has non-alphanumeric characters')
+    .optional({ checkFalsy: true }),
+  body('birth_name').escape().optional({ checkFalsy: true }),
+  body('mini_bio').escape().optional({ checkFalsy: true }),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.render('director_form', {
+        title: 'Create Director',
+        director: req.body,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      let director = new Director({
+        first_name: req.body.first_name,
+        family_name: req.body.family_name,
+        date_of_birth: req.body.date_of_birth,
+        date_of_death: req.body.date_of_death,
+        place_of_birth: req.body.place_of_birth,
+        place_of_death: req.body.place_of_death,
+        birth_name: req.body.birth_name,
+        mini_bio: req.body.mini_bio,
+      });
+      director.save((err) => {
+        if (err) {
+          return next(err);
+        }
+        res.redirect(director.url);
+      });
+    }
+  },
+];
 
 exports.director_delete_get = (req, res) => {
   res.send('delete director get');
